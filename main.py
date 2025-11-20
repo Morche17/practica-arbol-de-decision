fdb4bd70d7be3192fe536dd3dfa955b6063249fc
Emanuel Tavares <tavares2003@tutanota.com>
2025-11-20 08:38:28 -0800

    Commit inicial. Programa funcional.

---
+++ main.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_wine
+from sklearn.tree import DecisionTreeClassifier, export_text
+from sklearn.model_selection import train_test_split
+
+# Cargar dataset
+wine = load_wine()
+X, y = wine.data, wine.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Crear y entrenar el modelo
+tree = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree.fit(X_train, y_train)
+
+# Generando reglas del árbol
+rules = export_text(tree, feature_names=wine.feature_names)
+print(rules)
+
+exactitud = tree.score(X_test, y_test)
+print("~ La precisión fue de:", exactitud)
