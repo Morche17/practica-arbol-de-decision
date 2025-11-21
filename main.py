e49cb8a4018ba5a3bcedd380e8cadfe485a0e210
Emanuel Tavares <tavares2003@tutanota.com>
2025-11-21 09:59:36 -0800

    Falta dar opinión sobre mi base del conocimiento.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ X, y = wine.data, wine.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Crear y entrenar el modelo
-tree = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree = DecisionTreeClassifier(max_depth=4, random_state=42)
 tree.fit(X_train, y_train)
 
 # Generando reglas del árbol
